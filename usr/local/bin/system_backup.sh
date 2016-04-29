7d5730b409e5be1bf6fa9470cbf26d3d284881bd
tlaporte <travislaporte@gmail.com>
2016-04-29 15:52:09 -0400

    added packaged list

--- usr/local/bin/system_backup.sh
+++ usr/local/bin/system_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/CHESS","/etc/openvpn","/home/travis/.local/share/Steam"} / "/CHESS/Backups/Desktop Backups/tulkasBackup"
+rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/CHESS","/etc/openvpn","/home/travis/.local/share/Steam"} / "/CHESS/Backups/Desktop Backups/tulkasBackup"
