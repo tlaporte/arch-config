b3bcae92c2302206dd340b7379b9c6be56d820b1
tlaporte <travislaporte@gmail.com>
2016-06-18 06:37:59 -0400

    Updates

--- home/travis/.bash_profile
+++ home/travis/.bash_profile
@@ -2,6 +2,5 @@
 # ~/.bash_profile
 #
 
-setleds -D +num
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
--- usr/local/bin/system_backup.sh
+++ usr/local/bin/system_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -aAX --info=progress2 --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/CHESS","/etc/openvpn","/home/travis/.local/share/Steam"} / "/CHESS/Backups/Desktop Backups/tulkasBackup"
+rsync -aAXc --info=progress2 --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/CHESS","/etc/openvpn","/home/travis/.local/share/Steam"} / "/CHESS/Backups/Desktop Backups/tulkasBackup"
